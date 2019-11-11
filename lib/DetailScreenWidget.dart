0f5ef0f366733c4549530a79b7a049677a90296b
klepto <judeosby@gmail.com>
2019-11-11 14:39:13 +0530

    Completed Design

--- lib/DetailScreenWidget.dart
+++ lib/DetailScreenWidget.dart
@@ -5,7 +5,7 @@ import 'package:music_app/custom/nike_icons_icons.dart';
 import 'package:music_app/dummy/DataManager.dart';
 
 import 'ItemTile.dart';
-
+ScreenUtil instance = ScreenUtil.getInstance();
 class DetailScreenWidget extends StatefulWidget {
   static const String routeName = "/details";
 
@@ -26,148 +26,229 @@ class _DetailScreenWidgetState extends State<DetailScreenWidget> {
         backgroundColor: Colors.white,
         elevation: 0.4,
         iconTheme: IconThemeData(
-          color: Colors.black
+            color: Colors.black
         ),
         title: Container(
-           child: SvgPicture.asset("assets/images/NIKE.svg")
+            child: SvgPicture.asset("assets/images/NIKE.svg")
         ),
         centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child:Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          SizedBox.fromSize(
-            size: Size.square(MediaQuery.of(context).size.width),
-            child: PageView.builder(itemBuilder: (context,index){
-              if(index == 0){
-                return Image.asset(itemData.photoUrl,
-                  fit: BoxFit.fitWidth,
-                );
-              }
-              else{
-                return Image.asset("assets/images/shoes/$index.png",
-                  fit: BoxFit.fitWidth,);
-              }
-            },
-            scrollDirection: Axis.horizontal,
-            itemCount: 7,
-            onPageChanged: (index){
-              setState(() {
-                _scrollIndicatorSelectedPosition = index;
-              });
-            },
-            ),
-          ),
-          Divider(),
-          SizedBox.fromSize(
-            size: Size(MediaQuery.of(context).size.width,100),
-            child: ListView.builder(itemBuilder: (context,index){
-              return Image.asset("assets/images/shoes/$index.png",
-                fit: BoxFit.fitWidth,);
-            },
-            scrollDirection: Axis.horizontal,
-            itemCount: 4,),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        child: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text("NIKE AIR MAX SUPERSKY",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 23,
-                    ),),
-                    Text("Men's Running Shoe",
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 19,
-                    ),),
+                SizedBox.fromSize(
+                  size: Size.square(MediaQuery.of(context).size.width),
+                  child: PageView.builder(itemBuilder: (context,index){
 
-                  ],
+                    return Image.asset("assets/images/shoes/shoe-large.png",
+                      fit: BoxFit.fitWidth,);
+
+                  },
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 7,
+                    onPageChanged: (index){
+                      setState(() {
+                        _scrollIndicatorSelectedPosition = index;
+                      });
+                    },
+                  ),
                 ),
-                Text("\$${itemData.price}",
-                style: TextStyle(
-                  color: Color(0xff6858C8),
-                  fontSize: 23,
-                ),)
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-
-          Padding(
-            padding:  const EdgeInsets.symmetric(horizontal: 25),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                    child:Container(
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border.all(color:Colors.grey[300])
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: (Icon(NikeIcons.doted_heart,
-                          color: Color(0xff6858C8),)),
-                        ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: instance.setHeight(10),
+                  child: Center(
+                    child: SizedBox.fromSize(
+                      size: Size(instance.setWidth(200), instance.setHeight(2)),
+                      child: ListView.builder(itemBuilder: (context,index){
+                        return Icon(Icons.minimize,
+                            color: (index == _scrollIndicatorSelectedPosition)?Colors.black:Colors.grey[300]);
+                      },
+                        itemCount: 7,
+                        scrollDirection: Axis.horizontal,
                       ),
                     ),
+                  ),
                 ),
-                SizedBox(width: 20,),
-                Expanded(
-                  flex: 8,
-                    child:Container(
-                      decoration: BoxDecoration(
-                        border: Border.fromBorderSide(BorderSide(
-                            color:Colors.grey[300]
-                        ))
-                      ),
-                      child: DropdownButton(
-                        underline: null,
-                        hint: Text("Select your size"),
-                        items: [
-                       DropdownMenuItem(child: Text("S")),
-                       DropdownMenuItem(child: Text("M")),
-                       DropdownMenuItem(child: Text("L")),
-                       DropdownMenuItem(child: Text("XL")),
-                       DropdownMenuItem(child: Text("XXL")),
-                      ], onChanged: (item){
-
+                SizedBox(height: 15,),
+                Divider(),
+                SizedBox.fromSize(
+                  size: Size(MediaQuery.of(context).size.width,121),
+                  child: ListView.builder(itemBuilder: (context,index){
+                    int imageIndex = index+1;
+                    return GestureDetector(
+                      onTap: (){
+                        Navigator.pushNamed(context, DetailScreenWidget.routeName,arguments: {
+                          "id":itemData.itemId,
+                        });
                       },
+                      child: Image.asset("assets/images/shoes/$imageIndex.png",
+                        fit: BoxFit.fitWidth,),
+                    );
+                  },
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,),
+                ),
+                SizedBox(
+                  height: instance.setHeight(50),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("NIKE AIR MAX SUPERSKY",
+                            style: TextStyle(
+                              color: Color(0xff0A1734),
+                              fontSize: instance.setSp(18),
+                            ),),
+                          Text("Men's Running Shoe",
+                            style: TextStyle(
+                              color: Color(0xff9A9EB7),
+                              fontSize: instance.setSp(16),
+                            ),),
+
+                        ],
                       ),
-                    ) ),
+                      Text("\$${itemData.price}",
+                        style: TextStyle(
+                          color: Color(0xff6858C8),
+                          fontSize: 23,
+                        ),)
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+
+                Padding(
+                  padding:  const EdgeInsets.symmetric(horizontal: 25),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 1,
+                        child:Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              border: Border.all(color:Colors.grey[300])
+                          ),
+                          child: SizedBox.fromSize(
+                            size: Size(instance.setWidth(40),instance.setWidth(40)),
+                            child: Center(
+                              child: Icon(NikeIcons.doted_heart,
+                                size: instance.setWidth(20),
+                                color: Color(0xff6858C8),),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 20,),
+                      Expanded(
+                          flex: 8,
+                          child:SizeSelectionDropdown()),
+                    ],
+                  ),
+                ),
+                SizedBox.fromSize(
+                  size: Size.fromHeight(instance.setHeight(65)),
+                )
               ],
             ),
-          )
-        ],
           ),
+        ),
       ),
-
+      floatingActionButton: getAddItemButton(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 
 }
 
-// ListView.builder(itemBuilder: (context,index){
-//              return Icon(Icons.minimize,
-//                  color: (index == _scrollIndicatorSelectedPosition)?Colors.black:Colors.grey[300]);
-//            },
-//              itemCount: 7,
-//              scrollDirection: Axis.horizontal,
-//            )
+Widget getAddItemButton(BuildContext context){
+  return SizedBox.fromSize(
+    size: Size(MediaQuery.of(context).size.width, instance.setHeight(61)),
+    child: FlatButton(onPressed: (){
+
+    }, child: Center(
+      child:Text(
+        "ADD",
+      style: TextStyle(
+          fontSize: 14,
+        color: Colors.white
+      ),
+      ),
+    ),
+        color: Color(0xff594CA9),
+
+    ),
+  );
+}
+
+class SizeSelectionDropdown extends StatefulWidget {
+  final List<String> sizeOptions = ["S","M","L","XL"];
+
+  @override
+  _SizeSelectionDropdownState createState() => _SizeSelectionDropdownState();
+}
+
+class _SizeSelectionDropdownState extends State<SizeSelectionDropdown> {
+  String selectedSizeOption = "";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: instance.setHeight(40),
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(
+              color:Colors.grey[300]
+          ))
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal:16.0),
+        child: DropdownButtonHideUnderline(
+          child: DropdownButtonFormField<String>(
+            icon: Icon(Icons.keyboard_arrow_down,
+              color: Color(0xff594CA9),),
+            decoration: InputDecoration.collapsed(hintText:""),
+            hint: Text("SIZE",
+              style: TextStyle(
+                fontSize: instance.setSp(14),
+                color: Color(0xff9A9EB7),
+
+              ),
+            ),
+
+            items: widget.sizeOptions.map((String option){
+              if(selectedSizeOption == ""){
+                selectedSizeOption = option;
+              }
+              return DropdownMenuItem(
+                child: Text(option),
+                value: option
+              );
+            }).toList(),
+            onChanged: (size){
+            setState(() {
+              selectedSizeOption = size;
+            });
+          },
+            value: selectedSizeOption,
+          ),
+        ),
+      ),
+    ) ;
+  }
+}
+
+
+
+
 
 
